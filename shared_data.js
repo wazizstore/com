442c7bfe01c9b4433a1e09ca5d9e50043d5de744
wazizstore <chatom2007@gmail.com>
2026-05-08 15:54:28 +0200

    Add files via upload

---
+++ shared_data.js
@@ -0,0 +1,78 @@
+const products = [
+  {
+    id: 17,
+    name: "Ensemble Classic",
+    price: "199",
+    originalPrice: "229",
+    description: "طقم أنيق خامات فاخرة وتفصيل محترف.",
+    images: [
+      "images/6032874041250614618.jpg",
+      "images/6032874041250614620.jpg",
+      "images/6032874041250614622.jpg"
+    ],
+    colors: [
+      {name:"أسود", class:"black", inStock: true},
+      {name:"أبيض", class:"white", inStock: true},
+      {name:"ازرق", class:"blue", inStock: true}
+    ],
+    sizes: ["M","L","XL"],
+    inStock: true
+  },
+  {
+    id: 13,
+    name: "Ensemble Premium",
+    price: "219",
+    originalPrice: "259",
+    description: "طقم فاخر من قميص وبنطلون، تصميم عصري، مناسب للسهرات والمناسبات الخاصة.",
+    images: [
+      "images/Untitled design (9).jpg",
+      "images/Untitled design (70).png",
+      "images/Untitled design (71).png"
+    ],
+    colors: [
+      {name:"أسود", class:"black", inStock: true},
+      {name:"أبيض", class:"white", inStock: true},
+      {name:"بني", class:"brown", inStock: true}
+    ],
+    sizes: ["M","L","XL"],
+    inStock: true
+  },
+  {
+    id: 10,
+    name: "Ensemble Luxe",
+    price: "199",
+    originalPrice: "249",
+    description: "طقم شتوي دافئ، مناسب لأيام البرد. خامة ثقيلة وعالية الجودة. تصميم أنيق وعصري.",
+    images: [
+      "images/photo_2026-04-04_20-57-57 (2).jpg",
+      "images/photo_2026-04-04_20-59-18.jpg",
+      "images/photo_2026-04-04_20-58-16.jpg"
+    ],
+    colors: [
+      {name:"أسود", class:"black", inStock: true},
+      {name:"أبيض", class:"white", inStock: true},
+      {name:"فضي", class:"silver", inStock: true}
+    ],
+    sizes: ["M","L","XL"],
+    inStock: true
+  },
+  {
+    id: 18,
+    name: "Ensemble Modern",
+    price: "199",
+    originalPrice: "229",
+    description: "طقم أنيق خامات فاخرة وتفصيل محترف.",
+    images: [
+      "images/1754420532_IMG_20250713_230913_861.jpg",
+      "images/1754420532_IMG_20250713_230913_901.jpg",
+      "images/1754420532_IMG_20250713_230914_218.jpg"
+    ],
+    colors: [
+      {name:"أسود", class:"black", inStock: true},
+      {name:"أبيض", class:"white", inStock: true},
+      {name:"ازرق", class:"blue", inStock: true}
+    ],
+    sizes: ["M","L","XL"],
+    inStock: true
+  }
+];

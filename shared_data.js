f33d9ba5da96900ea99897204545fff1e68c0f47
wazizstore <chatom2007@gmail.com>
2026-05-08 16:09:07 +0200

    Update shared_data.js

--- shared_data.js
+++ shared_data.js
@@ -6,9 +6,9 @@ const products = [
     originalPrice: "229",
     description: "طقم أنيق خامات فاخرة وتفصيل محترف.",
     images: [
-      "images/6032874041250614618.jpg",
-      "images/6032874041250614620.jpg",
-      "images/6032874041250614622.jpg"
+      "6032874041250614618.jpg",
+      "6032874041250614620.jpg",
+      "6032874041250614622.jpg"
     ],
     colors: [
       {name:"أسود", class:"black", inStock: true},
@@ -25,9 +25,9 @@ const products = [
     originalPrice: "259",
     description: "طقم فاخر من قميص وبنطلون، تصميم عصري، مناسب للسهرات والمناسبات الخاصة.",
     images: [
-      "images/Untitled design (9).jpg",
-      "images/Untitled design (70).png",
-      "images/Untitled design (71).png"
+      "Untitled design (9).jpg",
+      "Untitled design (70).png",
+      "Untitled design (71).png"
     ],
     colors: [
       {name:"أسود", class:"black", inStock: true},
@@ -44,9 +44,9 @@ const products = [
     originalPrice: "249",
     description: "طقم شتوي دافئ، مناسب لأيام البرد. خامة ثقيلة وعالية الجودة. تصميم أنيق وعصري.",
     images: [
-      "images/photo_2026-04-04_20-57-57 (2).jpg",
-      "images/photo_2026-04-04_20-59-18.jpg",
-      "images/photo_2026-04-04_20-58-16.jpg"
+      "photo_2026-04-04_20-57-57 (2).jpg",
+      "photo_2026-04-04_20-59-18.jpg",
+      "photo_2026-04-04_20-58-16.jpg"
     ],
     colors: [
       {name:"أسود", class:"black", inStock: true},
@@ -63,9 +63,9 @@ const products = [
     originalPrice: "229",
     description: "طقم أنيق خامات فاخرة وتفصيل محترف.",
     images: [
-      "images/1754420532_IMG_20250713_230913_861.jpg",
-      "images/1754420532_IMG_20250713_230913_901.jpg",
-      "images/1754420532_IMG_20250713_230914_218.jpg"
+      "1754420532_IMG_20250713_230913_861.jpg",
+      "1754420532_IMG_20250713_230913_901.jpg",
+      "1754420532_IMG_20250713_230914_218.jpg"
     ],
     colors: [
       {name:"أسود", class:"black", inStock: true},
